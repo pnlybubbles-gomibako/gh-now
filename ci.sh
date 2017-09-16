37c01766ec128e24107d30b2decce53d5f1f62f7
pnlybubbles <pnlybubbles@gmail.com>
2017-09-16 22:06:31 +0900

    fix: ci freezing

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@ git config --global user.name ${GIT_NAME}
 git config --global user.email ${GIT_EMAIL}
 if git branch | grep -w gh-pages > /dev/null 2>&1; then git branch -D gh-pages; fi
 git checkout --orphan gh-pages
-now rm $INSTANCE_NAME -t $NOW_TOKEN
+yes | now rm $INSTANCE_NAME -t $NOW_TOKEN
 url=`now --public -t $NOW_TOKEN`
 npm run clean
 env NOW_BASE=$url npm run build:gh
